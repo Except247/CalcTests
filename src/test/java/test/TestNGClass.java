0fbc7e5f000c5fe4acda75e873bd5c35a3ebce2c
Mellian <aaperevozn@mts.ru>
2023-09-21 01:13:12 +0300

    fix

--- src/test/java/test/TestNGClass.java
+++ src/test/java/test/TestNGClass.java
@@ -1,18 +1,14 @@
 package test;
 
-import org.junit.After;
-import org.junit.runners.Parameterized;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.text.DecimalFormat;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.*;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -28,9 +24,13 @@ public class TestNGClass
     public void setUp(String browser) {
         if (browser.equalsIgnoreCase("firefox"))
         {
-            driver = new FirefoxDriver();
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
+            firefoxOptions.setPageLoadStrategy(PageLoadStrategy.EAGER);
+            driver = new FirefoxDriver(firefoxOptions);
         }else if (browser.equalsIgnoreCase("chrome")){
-            driver = new ChromeDriver();
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.setPageLoadStrategy(PageLoadStrategy.EAGER);
+            driver = new ChromeDriver(chromeOptions);
         }
         js = (JavascriptExecutor) driver;
         driver.get("https://www.calculator.net/");
@@ -38,92 +38,71 @@ public class TestNGClass
 
     @Test
     public void test() {
-        // Test name: Test_summ
-        // Step # | name | target | value
-        // 1 | open | / |
-        // 2 | setWindowSize | 1054x808 |
         driver.manage().window().setSize(new Dimension(1054, 808));
-        // 3 | click | css=div:nth-child(2) > a > img |
-
         driver.findElement(By.cssSelector("div:nth-child(2) > a > img")).click();
-
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-
         driver.findElement(By.id("calcSearchTerm")).click();
-        // 5 | click | id=calcSearchTerm |
         driver.findElement(By.id("calcSearchTerm")).click();
-        // 6 | type | id=calcSearchTerm | Scientific Calculator
         driver.findElement(By.id("calcSearchTerm")).sendKeys("Scientific Calculator");
-        // 7 | click | css=#calcSearchOut a |
         driver.findElement(By.cssSelector("#calcSearchOut a")).click();
-        // 4 | click | linkText=Scientific Calculator |
-        //driver.findElement(By.linkText("Scientific Calculator")).click();
-        // 5 | click | css=div:nth-child(1) > .scinm:nth-child(2) |
         driver.findElement(By.cssSelector("div:nth-child(1) > .scinm:nth-child(2)")).click();
-        // 6 | click | css=div:nth-child(3) > .sciop:nth-child(4) |
         driver.findElement(By.cssSelector("div:nth-child(3) > .sciop:nth-child(4)")).click();
-        // 7 | click | css=div:nth-child(5) > .scifunc:nth-child(1) |
         driver.findElement(By.cssSelector("div:nth-child(5) > .scifunc:nth-child(1)")).click();
-        // 8 | click | css=div:nth-child(1) > .scinm:nth-child(2) |
         driver.findElement(By.cssSelector("div:nth-child(1) > .scinm:nth-child(2)")).click();
-        // 9 | click | css=div:nth-child(2) > .sciop:nth-child(4) |
         driver.findElement(By.cssSelector("div:nth-child(2) > .sciop:nth-child(4)")).click();
-        // 10 | click | css=div:nth-child(3) > .scinm:nth-child(3) |
         driver.findElement(By.cssSelector("div:nth-child(3) > .scinm:nth-child(3)")).click();
-        // 11 | click | css=div:nth-child(5) > .scifunc:nth-child(2) |
         driver.findElement(By.cssSelector("div:nth-child(5) > .scifunc:nth-child(2)")).click();
-        // 12 | click | css=.scieq:nth-child(4) |
         driver.findElement(By.cssSelector(".scieq:nth-child(4)")).click();
 
         assertThat(driver.findElement(By.id("sciOutPut")).getText(), is(" " + 8 * (8 - 3)));
-        // 13 | click | css=.scieq:nth-child(3) |
-        driver.findElement(By.cssSelector(".scieq:nth-child(3)")).click();
-        // 14 | click | css=div:nth-child(4) > .scifunc:nth-child(5) |
+
+        new Actions(driver).sendKeys(Keys.ESCAPE);
+
         driver.findElement(By.cssSelector("div:nth-child(4) > .scifunc:nth-child(5)")).click();
-        // 6 | click | css=div:nth-child(2) > .scinm:nth-child(2) |
-        driver.findElement(By.cssSelector("div:nth-child(2) > .scinm:nth-child(2)")).click();
-        // 7 | click | css=div:nth-child(2) > .scinm:nth-child(3) |
-        driver.findElement(By.cssSelector("div:nth-child(2) > .scinm:nth-child(3)")).click();
-        // 8 | click | css=div:nth-child(5) > .scifunc:nth-child(2) |
-        driver.findElement(By.cssSelector("div:nth-child(5) > .scifunc:nth-child(2)")).click();
-        // 9 | click | css=div:nth-child(4) > .sciop:nth-child(4) |
-        driver.findElement(By.cssSelector("div:nth-child(4) > .sciop:nth-child(4)")).click();
-        // 10 | click | css=div:nth-child(4) > .scifunc:nth-child(4) |
+
+        new Actions(driver)
+                .sendKeys("56")
+                .keyDown(Keys.SHIFT)
+                .sendKeys("0")
+                .keyUp(Keys.SHIFT)
+                .sendKeys(Keys.DIVIDE)
+                .perform();
+
         driver.findElement(By.cssSelector("div:nth-child(4) > .scifunc:nth-child(4)")).click();
-        // 11 | click | css=div:nth-child(2) > .scinm:nth-child(2) |
-        driver.findElement(By.cssSelector("div:nth-child(2) > .scinm:nth-child(2)")).click();
-        // 12 | click | css=div:nth-child(2) > .scinm:nth-child(3) |
-        driver.findElement(By.cssSelector("div:nth-child(2) > .scinm:nth-child(3)")).click();
-        // 13 | click | css=div:nth-child(5) > .scifunc:nth-child(2) |
-        driver.findElement(By.cssSelector("div:nth-child(5) > .scifunc:nth-child(2)")).click();
-        // 14 | click | css=div:nth-child(3) > .sciop:nth-child(4) |
-        driver.findElement(By.cssSelector("div:nth-child(3) > .sciop:nth-child(4)")).click();
-        // 15 | click | css=div:nth-child(5) > .scifunc:nth-child(1) |
-        driver.findElement(By.cssSelector("div:nth-child(5) > .scifunc:nth-child(1)")).click();
-        // 16 | click | css=div:nth-child(2) > .scinm:nth-child(2) |
-        driver.findElement(By.cssSelector("div:nth-child(2) > .scinm:nth-child(2)")).click();
-        // 17 | click | css=div:nth-child(4) > .scifunc:nth-child(3) |
+
+        new Actions(driver)
+                .sendKeys("56")
+                .keyDown(Keys.SHIFT)
+                .sendKeys("0")
+                .keyUp(Keys.SHIFT)
+                .sendKeys(Keys.MULTIPLY)
+                .keyDown(Keys.SHIFT)
+                .sendKeys("9")
+                .keyUp(Keys.SHIFT)
+                .sendKeys("5")
+                .sendKeys(Keys.MULTIPLY)
+                .perform();
+
         driver.findElement(By.cssSelector("div:nth-child(4) > .scifunc:nth-child(3)")).click();
-        // 18 | click | css=div:nth-child(2) > .scinm:nth-child(1) |
-        driver.findElement(By.cssSelector("div:nth-child(2) > .scinm:nth-child(1)")).click();
-        // 19 | click | css=div:nth-child(5) > .scifunc:nth-child(2) |
-        driver.findElement(By.cssSelector("div:nth-child(5) > .scifunc:nth-child(2)")).click();
-        // 20 | click | css=div:nth-child(3) > .sciop:nth-child(4) |
-        driver.findElement(By.cssSelector("div:nth-child(3) > .sciop:nth-child(4)")).click();
-        // 21 | click | css=div:nth-child(3) > .scinm:nth-child(1) |
-        driver.findElement(By.cssSelector("div:nth-child(3) > .scinm:nth-child(1)")).click();
-        // 22 | click | css=div:nth-child(4) > .scinm:nth-child(1) |
-        driver.findElement(By.cssSelector("div:nth-child(4) > .scinm:nth-child(1)")).click();
-        // 23 | click | css=div:nth-child(5) > .scifunc:nth-child(2) |
-        driver.findElement(By.cssSelector("div:nth-child(5) > .scifunc:nth-child(2)")).click();
-        // 24 | click | css=.scieq:nth-child(4) |
+
+        new Actions(driver)
+                .sendKeys("4")
+                .keyDown(Keys.SHIFT)
+                .sendKeys("0")
+                .keyUp(Keys.SHIFT)
+                .sendKeys(Keys.MULTIPLY)
+                .sendKeys("10")
+                .keyDown(Keys.SHIFT)
+                .sendKeys("0")
+                .keyUp(Keys.SHIFT)
+                .sendKeys(Keys.ENTER)
+                .perform();
+
         driver.findElement(By.cssSelector(".scieq:nth-child(4)")).click();
         double d = Math.log10(56)/Math.log(56)*(5*Math.sqrt(4)*10);
         DecimalFormat df = new DecimalFormat("0.########");
         String outpoutString = df.format(d).replace(",", ".");
-
-        //Math.log10(56)/Math.log(56)*(5*Math.sqrt(4)*10)
 
         assertThat(driver.findElement(By.id("sciOutPut")).getText(), is(" " + outpoutString));
     }
